bcb5f4381ba978e2d77820204226b99385d7ef92
itiichai <itiichar@ais.co.th>
2024-11-06 08:47:21 +0000

    web api2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello World - 1.0.0');
+    res.send('Hello World - 1.0.1');
 });
 app.listen(PORT,HOST,() => {
     console.log(`Running On http://${HOST}:${PORT}`);
